c903432ecb8b4eb5c25f5412c1ba55419cd71c7f
unknown <paul7markets@hotmail.com>
2023-07-19 12:38:13 +0100

    navbar created

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
 });
+
+function onHamburgerClicked(){
+  const navbar = document.getElementById("navbar");
+  if(navbar.style.display === "flex"){
+    navbar.style.display = "none";
+  } else{
+    navbar.style.display = "flex";
+  }
+}
